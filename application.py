5f4987cd42b2feccc4fa3a5f0e77a265f88d1f4f
Aditya Kulkarni <kulkarniaditya1002@gmail.com>
2022-12-13 00:57:21 -0500

    Flask_backend_code_push_1.0

---
+++ application.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request
+from PIL import Image
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the uploaded image file
+    image = request.files['image']
+
+    # Open the image using the Pillow library
+    img = Image.open(image)
+
+    # Process the image to extract any relevant information
+    # ...
+
+    return render_template('result.html', result=result)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the uploaded files
+    uploaded_files = request.files.getlist("files")
+
+    # Iterate over the uploaded files and save them to the desired location
+    for file in uploaded_files:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join('/path/to/save/folder', filename))
+
+# Using Inception V3
+import tensorflow as tf
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+
+# Load the Inception V3 model
+model = InceptionV3(weights='imagenet')
+
+# Load the input image from the PDF document
+input_image = load_image_from_pdf('document.pdf')
+
+# Pre-process the input image for the Inception V3 model
+input_image = preprocess_input(input_image)
+
+# Use the Inception V3 model to predict the class of the input image
+predictions = model.predict(input_image)
+
+# Find the image in the PDF document that most closely matches the input image
+# using the predicted class and the Inception V3 model's feature vector
+matching_image = find_matching_image(predictions, 'document.pdf')
+
+# Print the file path of the matching image
+print(matching_image)
